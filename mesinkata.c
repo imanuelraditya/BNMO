6a9cfe78a00def3576dbb8cec546c1e8437fd7e1
michaelsht <16521201@mahasiswa.itb.ac.id>
2022-10-31 22:50:45 +0700

    revisi ADT

--- mesinkata.c
+++ mesinkata.c
@@ -46,3 +46,63 @@ void CopyWord() {
     }
     currentWord.Length = i;
 }
+
+void advNewline()
+{
+    Word EMPTY = {"", 0};
+    currentWord = EMPTY;
+    if(currentChar == MARK){
+        EndWord = false;
+        ADV();
+        copyWord();
+    }
+}
+
+Word concatWord(Word w1, Word w2)
+{
+    Word ans;
+    ans.Length = w1.Length + w2.Length;
+    int i;
+    for(i = 0; i < w1.Length; i++){
+        ans.TabWord[i] = w1.TabWord[i];
+    }
+    for(i = 0; i < w2.Length;i++){
+        ans.TabWord[i+w1.Length] = w2.TabWord[i];
+    }
+    return ans;
+}
+
+boolean isWordEqual(Word a, Word b)
+{
+    if(a.Length != b.Length){
+        return false;
+    }
+    else{
+        int i = 0;
+        boolean flag = true;
+        while(i < a.Length && flag){
+            if(a.TabWord[i] != b.TabWord[i]){
+                flag = false;
+            }
+            i++;
+        }
+        return flag;
+    }
+}
+
+void displayWord(Word w)
+{
+    int i;
+    for(i = 0; i<w.Length;i++){
+        printf("%c", w.TabWord[i]);
+    }
+    printf("\n");
+}
+
+void copyWordFromWord(Word w1, Word *w2)
+{
+    w2->Length = w1.Length;
+    for(int i = 0; i < w1.Length; i++){
+        w2->TabWord[i] = w1.TabWord[i];
+    }
+}
