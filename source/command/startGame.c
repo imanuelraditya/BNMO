4f4d11f90135bb5c0caab820d16c8abefc022b0f
miralistyacahya <16521207@mahasiswa.itb.ac.id>
2022-11-02 19:57:27 +0700

    start versi agak ril

--- source/command/startGame.c
+++ source/command/startGame.c
@@ -14,10 +14,16 @@ void startGame() {
         jumlah = (jumlah * 10 + (currentWord.TabWord[i] - '0'));
     }
 
-    printf("%d\n", jumlah);
+    printf("%d\n", jumlah); //ini kebawah cuma buat ngetes
     printf("%d\n", jumlah + 7);
     advNewlineFile();
     printf("%s\n", currentWord.TabWord);
     advNewlineFile();
     printf("%s\n", currentWord.TabWord);
+
+    // ini start game versi beneran kalo uda ada listnya
+    // for (i = 0 ; i < jumlah ; i++) {
+    //     advNewlineFile();
+    //     currentWord.TabWord() dimasukin ke list game
+    // }
 }
