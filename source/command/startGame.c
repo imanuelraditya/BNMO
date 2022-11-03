767cbf85f46436ce9e283d9cc6cee971e75979e7
miralistyacahya <16521207@mahasiswa.itb.ac.id>
2022-11-03 17:57:53 +0700

    rapiin start

--- source/command/startGame.c
+++ source/command/startGame.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include "startGame.h"
 
-void startGame(ArrayDin * arr) {
-    int jumlah = 0;
-    int i = 0;
+void startGame(ArrayDin * arr)
+/* membuka dan membaca file konfigurasi awal */
+/* I.S. sembarang (tidak dapat menggunakan game)
+   F.S. game sudah siap dimainkan
+        List Game yang dapat dimainkan sudah tersedia yaitu dengan konfigurasi awal
+        command - command lain dapat dijalankan */
+{
+    int jumlah = 0; // jumlah game yang tersedia, untuk looping banyak membaca 
+    int i = 0; 
 
-    startWFile("data/config.txt");
-    
-    //printf("%s\n", currentWord.TabWord);
-    //printf("%d\n", currentWord.Length);
+    startWFile("data/config.txt"); // membuka file konfigurasi awal yang tersedia di dalam file config.txt
+
+    /* currentWord adalah baris pertama pada file config.txt
+       baris pertama adalah jumlah game yang tersedia -> angka -> diubah dari string menjadi integer, 
+       karena nanti akan digunakan untuk looping sesuai jumlah tersebut */
 
     for(i = 0 ; i < currentWord.Length ; i++) {
-        jumlah = (jumlah * 10 + (currentWord.TabWord[i] - '0'));
+        jumlah = (jumlah * 10 + (currentWord.TabWord[i] - '0'));  
     }
 
-    //printf("%d\n", jumlah); //ini kebawah cuma buat ngetes
-    //printf("%d\n", jumlah + 7);
-    // advNewlineFile();
-    // printf("%s\n", currentWord.TabWord);
-    // advNewlineFile();
-    // printf("%s\n", currentWord.TabWord);
-    // advNewlineFile();
-    // printf("%s\n", currentWord.TabWord);
-    // advNewlineFile();
-    // printf("%s\n", currentWord.TabWord);
-    // advNewlineFile();
-    // printf("%s\n", currentWord.TabWord);
-
-    // ini start game versi beneran kalo uda ada listnya
+    // memasukkan semua judul game pada file konfigurasi awal sebanyak 'jumlah' kedalam list game
     for (i = 0 ; i < jumlah ; i++) {
          advNewlineFile();
          InsertLast(arr, currentWord);
--- source/command/startGame.h
+++ source/command/startGame.h
@@ -4,3 +4,7 @@
 
 void startGame(ArrayDin * arr);
 /* membuka dan membaca file konfigurasi awal */
+/* I.S. sembarang (tidak dapat menggunakan game)
+   F.S. game sudah siap dimainkan
+        List Game yang dapat dimainkan sudah tersedia yaitu dengan konfigurasi awal
+        command - command lain dapat dijalankan */
